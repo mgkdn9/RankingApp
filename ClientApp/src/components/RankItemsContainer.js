04f1a8cdc90d348db0d7ce9bb760480d5d5dae60
mgkdn9 <mkohlberg95@gmail.com>
2023-03-07 15:25:35 -0500

    initial commit

---
+++ ClientApp/src/components/RankItemsContainer.js
@@ -0,0 +1,32 @@
+﻿import {useState} from 'react'
+import RankItems from './RankItems';
+
+const RankItemsContainer = ({ dataType, imgArr }) => {
+    const movieLocalStorageKey = "movies"
+    const albumLocalStorageKey = "albums"
+
+    var localStorageKey = "";
+
+    const [movieItems, setMovieItems] = useState(JSON.parse(localStorage.getItem(movieLocalStorageKey)))
+    const [albumItems, setAlbumItems] = useState(JSON.parse(localStorage.getItem(albumLocalStorageKey)))
+
+    var data = []
+    var setFunc = null
+
+    if (dataType === 1) {
+        data = movieItems;
+        setFunc = setMovieItems;
+        localStorageKey = movieLocalStorageKey
+    }
+    else if (dataType === 2) {
+        data = albumItems;
+        setFunc = setAlbumItems;
+        localStorageKey = albumLocalStorageKey
+    }
+
+    return (
+        <RankItems items={data} setItems={setFunc} dataType={dataType} imgArr={imgArr} localStorageKey={localStorageKey} />
+    )
+
+}
+export default RankItemsContainer
